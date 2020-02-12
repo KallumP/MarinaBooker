77f4b30482b4601a037888f9f38caae3382dd941
Kallum Panditharatna <kallum179@gmail.com>
2020-02-12 21:55:33 +0000

    Linked list updated to now take pointers to addresses

--- source/MarinaBookingSystem/LinkedList.cpp
+++ source/MarinaBookingSystem/LinkedList.cpp
@@ -3,56 +3,55 @@
 
 LinkedList::LinkedList() {
 
+	//sets the start to point to nothing
+	start = nullptr;
 }
 
-void LinkedList::AddItem(ListItem toAdd) {
+void LinkedList::AddItem(ListItem newItem) {
+
+	//adds the new list item to the linked list
+	spawnOrder.push_back(newItem);
 
 	//checks if there was anything in the linked list already
-	if (spawnOrder.size() == 0)
+	if (start == nullptr)
 
-		SortFirstItem(toAdd);
+		SortFirstItem(&spawnOrder[spawnOrder.size() - 1]);
 
 	else
 
-		SortItem(toAdd, start);
+		SortItem(&spawnOrder[spawnOrder.size() - 1], start);
 }
 
 //Sorts the new item as a first item
-void LinkedList::SortFirstItem(ListItem toAdd) {
+void LinkedList::SortFirstItem(ListItem* newItem) {
 
-	//lets the linked list know to start at the this value
-	start = toAdd;
-
-	//lets the linked list know that the list ends at this value
-	end = toAdd;
-
-	//lets the linked list know that this is the last item
-	toAdd.SetNextPointer(nullptr);
+	//lets the linked list know that the new item is the last item
+	newItem->SetNextPointer(nullptr);
 
 	//lets the linked list know that there are no previous items
-	toAdd.SetPrevPointer(nullptr);
+	newItem->SetPrevPointer(nullptr);
 
-	//adds the listitem to the linked list
-	spawnOrder.push_back(toAdd);
+	//lets the linked list know to start at the new item
+	start = newItem;
 }
 
 //sorts the item into the list
-void LinkedList::SortItem(ListItem newItem, ListItem current) {
+void LinkedList::SortItem(ListItem* newItem, ListItem* current) {
+	
+	//checks to see if the  item being added goes before the current item being checked
+	if (newItem->GetBoat().GetEnter() < current->GetBoat().GetEnter()) {
 
-	//checks to see if the item being added goes before the current item being checked
-	if (newItem.GetBoat().GetEnter() < current.GetBoat().GetEnter()) {
+		//sets the new item's next pointer to point to the current item
+		newItem->SetNextPointer(current);
 
-		//sets the new item's next pointer to the current item's next pointer
-		newItem.SetNextPointer(&current);
+		//sets the new item's previous pointer to now point to the current item's previous pointer
+		newItem->SetPrevPointer(current->GetPrevPointer());
 
-		//sets the previous pointer to now point to the current item's pointer
-		newItem.SetPrevPointer(current.GetPrevPointer());
+		//sets the current item's previous pointer to point to the new item
+		current->SetPrevPointer(newItem);
 
-		//sets the current item to point to the new item
-		current.SetPrevPointer(&newItem);
-
-		//checks to see if the current item being checked was the first in the list
-		if (current.GetPrevPointer() == nullptr)
+		//checks to see if the current item being checked was the first in the linked list
+		if (current->GetPrevPointer() == nullptr)
 
 			//sets the starting point at the new item
 			start = newItem;
@@ -60,23 +59,20 @@ void LinkedList::SortItem(ListItem newItem, ListItem current) {
 	} else {
 
 		//checks to see if there were more items to sort through
-		if (current.GetNextPointer() != nullptr) {
+		if (current->GetNextPointer() != nullptr) {
 
 			//calls the sort function on the next item in the linked list
-			SortItem(newItem, *current.GetNextPointer());
+			SortItem(newItem, current->GetNextPointer());
 
 		} else {
 
-			//sets the current item to point to the new item
-			current.SetNextPointer(&newItem);
+			//sets the current item's next pointer to point to the new item
+			current->SetNextPointer(newItem);
 
 			//sets the new item's previous item pointer to point to the current item
-			newItem.SetPrevPointer(&current);
+			newItem->SetPrevPointer(current);
 		}
-
 	}
-
-	spawnOrder.push_back(newItem);
 }
 
 //goes through the deleting protocol
@@ -90,13 +86,13 @@ void LinkedList::Output() {
 	TraverseList(start);
 }
 
-void LinkedList::TraverseList(ListItem item) {
+void LinkedList::TraverseList(ListItem* item) {
 
 	//outputs the name and the entry time of the current item's boat
-	std::cout << "Boat: " << item.GetBoat().GetName() << " will be entering at: " << item.GetBoat().GetEnter() << std::endl;
+	std::cout << "Boat: " << item->GetBoat().GetName() << " will be entering at: " << item->GetBoat().GetEnter() << std::endl;
 
 	//checks to see if there were more in the list to traverse to
-	if (item.GetNextPointer() != nullptr)
-		TraverseList(*item.GetNextPointer());
+	if (item->GetNextPointer() != nullptr)
+		TraverseList(item->GetNextPointer());
 
 }
--- source/MarinaBookingSystem/LinkedList.h
+++ source/MarinaBookingSystem/LinkedList.h
@@ -11,18 +11,17 @@ public:
 	LinkedList();
 
 	void AddItem(ListItem toAdd);
-	void SortFirstItem(ListItem toAdd);
-	void SortItem(ListItem toAdd, ListItem current);
+	void SortFirstItem(ListItem* toAdd);
+	void SortItem(ListItem* toAdd, ListItem* current);
 
 	void DeleteItem();
 
 	void Output();
-	void TraverseList(ListItem item);
+	void TraverseList(ListItem* item);
 
 private:
 
 	std::vector<ListItem> spawnOrder;
-	ListItem start;
-	ListItem end;
+	ListItem* start;
 };
 
